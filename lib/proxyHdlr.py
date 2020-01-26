398b9bcadaab291ee378e1314f9ba06398b99ed3
dag <dag@localhost>
2020-01-26 22:05:30 +0100

    Added logging to "lib/proxyHdlr.py"
    Signed-off-by: dag <dag@localhost>

--- lib/proxyHdlr.py
+++ lib/proxyHdlr.py
@@ -6,9 +6,15 @@ This module contains the proxy handler function used by other modules.
 # Imports
 # ***************************************
 import socket
+import logging
 
 from lib.readBuffer import recvFrom
 from lib.reqResHdlrs import reqHdlr, resHdlr
+
+# *****************************************
+# Local variables
+# *****************************************
+_proxyHdlrLogger = logging.getLogger("monitoringProxy.main.proxyHdlr")
 
 
 # ***********************************************
@@ -17,13 +23,17 @@ from lib.reqResHdlrs import reqHdlr, resHdlr
 def proxyHdlr(clientSock):
     '''
     proxy handler process
-    '''            
+    '''
+    global _proxyHdlrLogger
+    
     while True:
         localBuffer = recvFrom(clientSock)
                         
         if len(localBuffer):
             localBuffer, remoteHost, remotePort = reqHdlr(localBuffer)
             
+            _proxyHdlrLogger.log(logging.INFO, "[ProxyHandler] " + localBuffer + " | " + remoteHost + ":" + remotePort)
+                       
             remoteSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             remoteSocket.connect((remoteHost, remotePort))
             remoteSocket.send(localBuffer)
